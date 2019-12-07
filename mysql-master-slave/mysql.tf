11d410c637cec5c86f95879c5382ad5797e3fe5c
Yury Stankevich <urykhy@gmail.com>
2019-12-07 21:47:51 +0300

    terraform mysql slave

---
+++ mysql-master-slave/mysql.tf
@@ -0,0 +1,78 @@
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_volume" "mysql-master" {
+	name = "mysql-master"
+}
+resource "docker_volume" "mysql-slave" {
+	name = "mysql-slave"
+}
+
+resource "docker_image" "mysql" {
+  name = "mysql:5.7"
+}
+
+resource "docker_container" "mysql-master" {
+  name  = "mysql-master"
+  image = docker_image.mysql.name
+  command = ["--server-id=1", "--log-bin=mysql-bin", "--binlog-format=ROW", "--gtid-mode=ON", "--enforce-gtid-consistency"]
+  env   = [
+    "MYSQL_ROOT_PASSWORD=root"
+  ]
+  volumes {
+    volume_name = docker_volume.mysql-master.name
+    container_path = "/var/lib/mysql"
+  }
+}
+
+resource "docker_container" "mysql-slave" {
+  name  = "mysql-slave"
+  image = docker_image.mysql.name
+  command = ["--server-id=2", "--log-bin=mysql-bin", "--binlog-format=ROW", "--gtid-mode=ON", "--enforce-gtid-consistency", "--log-slave-updates", "--read-only"]
+  env   = [
+    "MYSQL_ROOT_PASSWORD=root"
+  ]
+  volumes {
+    volume_name = docker_volume.mysql-slave.name
+    container_path = "/var/lib/mysql"
+  }
+}
+
+provider "mysql" {
+  endpoint = docker_container.mysql-master.ip_address
+  username = "root"
+  password = "root"
+}
+
+resource "mysql_user" "user" {
+  user               = "user"
+  host               = "%"
+  plaintext_password = ""
+}
+
+resource "mysql_grant" "master_user_grants" {
+  user       = mysql_user.user.user
+  host       = mysql_user.user.host
+  database   = "*"
+  privileges = ["CREATE","INSERT","SELECT","UPDATE","DELETE","DROP","INDEX","PROCESS"]
+}
+
+resource "mysql_user" "slave" {
+  user               = "slave"
+  host               = "%"
+  plaintext_password = "slave"
+}
+
+resource "mysql_grant" "master_slave_grants" {
+  user       = mysql_user.slave.user
+  host       = mysql_user.slave.host
+  database   = "*"
+  privileges = ["REPLICATION SLAVE"]
+}
+
+resource "null_resource" "start_slave" {
+    provisioner "local-exec" {
+        command = "mysql -proot -h \"${docker_container.mysql-slave.ip_address}\" -e \"CHANGE MASTER TO MASTER_HOST='${docker_container.mysql-master.ip_address}', MASTER_PORT=3306, MASTER_USER='${mysql_user.slave.user}', MASTER_PASSWORD='slave', MASTER_AUTO_POSITION=1; START SLAVE USER='${mysql_user.slave.user}' PASSWORD='slave'\""
+    }
+}
