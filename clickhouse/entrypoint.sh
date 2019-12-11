eb62ab7c2c35cc675696f0e668a5becd61f82866
Yury Stankevich <urykhy@gmail.com>
2019-12-11 20:12:26 +0300

    CH: fix

--- clickhouse/entrypoint.sh
+++ clickhouse/entrypoint.sh
@@ -4,5 +4,4 @@ HOST=`hostname -s`
 sed -i "s/%HOST%/$HOST/" /etc/metrika.xml
 sed -i "s/%SHARD%/$SHARD/" /etc/metrika.xml
 
-su clickhouse
-exec /usr/bin/clickhouse-server --config=/etc/clickhouse-server/config.xml
+exec su - -s /bin/bash -c '/usr/bin/clickhouse-server --config=/etc/clickhouse-server/config.xml' clickhouse
