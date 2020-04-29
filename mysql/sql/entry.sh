8c0c4a5c70fd424966dd06892e3a4eee38795ca7
Yury Stankevich <urykhy@gmail.com>
2020-04-29 22:50:20 +0300

    mysql: 8.0

--- mysql/sql/entry.sh
+++ mysql/sql/entry.sh
@@ -2,10 +2,10 @@
 
 mkdir -p     /data/tmp /data/sql
 chmod 777    /data/tmp /data/sql
-chown nobody /data/tmp /data/sql
+chown mysql -R /data/tmp /data/sql
 
 if [ -z "$(ls -A /data/sql)" ]; then
     echo "Initialize ..."
-    /opt/mysql/server-5.7/bin/mysqld --defaults-file=/etc/my.cnf -u nobody --initialize
+    mysqld --defaults-file=/etc/my.cnf -u mysql --initialize
 fi
-exec /opt/mysql/server-5.7/bin/mysqld --defaults-file=/etc/my.cnf -u nobody --skip-grant-tables
+exec mysqld --defaults-file=/etc/my.cnf -u mysql
