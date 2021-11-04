93a6fa45ac5f9c848a7835b5dfcdd3b6c65c7908
Yury Stankevich <urykhy@gmail.com>
2021-11-04 21:12:06 +0300

    kerberos

---
+++ kerberos/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+if [ ! -f /var/lib/krb5kdc/principal.ok ]; then
+    echo 'prepare ... '
+    kdb5_util -P kerberos create -s
+    echo -e 'admin\nadmin'   | kadmin.local -q "addprinc admin/admin"
+    echo -e 'ury\nury'       | kadmin.local -q "addprinc ury"
+    echo -e 'root\nroot'     | kadmin.local -q "addprinc root"
+    echo -e 'user\nuser'     | kadmin.local -q "addprinc user"
+    echo -e 'hadoop\nhadoop' | kadmin.local -q "addprinc hadoop/admin"
+fi
+
+exec /usr/bin/supervisord
