ac00e3bb46e9ef0d38386031a720413c2ef93942
Yury Stankevich <urykhy@gmail.com>
2022-02-26 20:18:17 +0300

    clickhouse: use gosu

--- clickhouse/entrypoint.sh
+++ clickhouse/entrypoint.sh
@@ -4,4 +4,4 @@ HOST=`hostname -s`
 sed -i "s/%HOST%/$HOST/" /etc/metrika.xml
 sed -i "s/%SHARD%/$SHARD/" /etc/metrika.xml
 
-exec su - -s /bin/bash -c '/usr/bin/clickhouse-server --config=/etc/clickhouse-server/config.xml' clickhouse
+exec gosu clickhouse /usr/bin/clickhouse server --config=/etc/clickhouse-server/config.xml
