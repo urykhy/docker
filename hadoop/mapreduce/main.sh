251a1196146e7ca4564c1c60e3a47810d040efd6
Yury Stankevich <urykhy@gmail.com>
2022-07-23 14:32:25 +0300

    hadoop/mapreduce: compression and combiner

--- hadoop/mapreduce/main.sh
+++ hadoop/mapreduce/main.sh
@@ -14,11 +14,17 @@ hadoop fs -put      /task/*.txt /root/task-input/
 hadoop fs -ls       /root/task-input
 
 hadoop jar /opt/hadoop-*/share/hadoop/tools/lib/hadoop-streaming-*.jar \
-    -file    /task/mapper.py    \
-    -mapper  /task/mapper.py    \
-    -file    /task/reducer.py   \
-    -reducer /task/reducer.py   \
-    -input   /root/task-input/* \
-    -output  /root/task-output
+    -Dmapreduce.map.output.compress=true \
+    -Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
+    -Dmapreduce.output.fileoutputformat.compress=true \
+    -Dmapreduce.output.fileoutputformat.compress.codec=org.apache.hadoop.io.compress.ZStandardCodec \
+    -Dmapreduce.output.fileoutputformat.compress.type=Block \
+    -file     /task/mapper.py    \
+    -mapper   /task/mapper.py    \
+    -file     /task/reducer.py   \
+    -combiner /task/reducer.py   \
+    -reducer  /task/reducer.py   \
+    -input    /root/task-input/* \
+    -output   /root/task-output
 
 hadoop fs -get /root/task-output/* /task/result/
