f3aa7c2b9590539134704e2949ec7a15d9758395
Yury Stankevich <urykhy@gmail.com>
2023-06-13 22:10:37 +0300

    hadoop: caching

--- hadoop/hadoop/build.sh
+++ hadoop/hadoop/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t urykhy/hadoop --build-arg WITH_KERBEROS=false .
+docker build -t urykhy/hadoop --build-arg WITH_KERBEROS=false --build-arg APT_PROXY=http://elf.dark:8081 --build-arg S_PROXY=http://elf.dark:8082 .
--- hadoop/spark/build.sh
+++ hadoop/spark/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t urykhy/hadoop-spark .
+docker build -t urykhy/hadoop-spark --build-arg S_PROXY=http://elf.dark:8082 .
