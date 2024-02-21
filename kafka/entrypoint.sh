3bc3101f57f89ce63643172167a0e0771fe325c8
Yury Stankevich <urykhy@gmail.com>
2024-02-21 21:27:33 +0300

    kafka: wait until zk node expired

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -i "s/%KAFKA_BROKER_ID%/${KAFKA_BROKER_ID:-1}/" /broker.properties
+while true; do ./zookeeper-shell.sh zookeeper ls /brokers/ids/${KAFKA_BROKER_ID} || break; echo "wait until ZK node expired .."; sleep 2; done
 exec ./kafka-server-start.sh /broker.properties
