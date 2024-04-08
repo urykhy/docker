39c1243c90a4d33266b5df8f064bb6ba95cf9df7
Yury Stankevich <urykhy@gmail.com>
2024-04-08 21:32:27 +0300

    kafka: RackAwareReplicaSelector

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed -i "s/%KAFKA_BROKER_ID%/${KAFKA_BROKER_ID:-1}/" /broker.properties
+while true; do ./zookeeper-shell.sh zookeeper version && break; echo "wait until ZK started .."; sleep 2; done
 while true; do ./zookeeper-shell.sh zookeeper ls /brokers/ids/${KAFKA_BROKER_ID} || break; echo "wait until ZK node expired .."; sleep 2; done
 exec ./kafka-server-start.sh /broker.properties
