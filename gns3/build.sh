624cfbdf2deccf5a99ca1ffeac7edf60bf3d9e33
Yury Stankevich <urykhy@gmail.com>
2024-06-08 09:21:20 +0300

    gns3: use debian as base image

--- gns3/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker stop gns3
-docker rm gns3
-docker build -t urykhy/gns3 .
--- gns3/start.sh
+++
@@ -1,2 +0,0 @@
-
-docker run -it --privileged --net=host -e LIBGL_ALWAYS_INDIRECT=1 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/machine-id:/etc/machine-id -v gns3:/home/user --name gns3 urykhy/gns3
