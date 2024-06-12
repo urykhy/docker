cd6bd37971681508ef6dac9aea9b70980f89584b
Yury Stankevich <urykhy@gmail.com>
2024-06-12 08:35:53 +0300

    goproxy

--- hadoop/hadoop/build.sh
+++ hadoop/hadoop/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker build -t urykhy/hadoop --build-arg WITH_KERBEROS=false --build-arg APT_PROXY=${APT_PROXY} --build-arg S_PROXY=${S_PROXY} .
+docker build -t urykhy/hadoop             \
+       --build-arg WITH_KERBEROS=false    \
+       --build-arg APT_PROXY=${APT_PROXY} \
+       --build-arg S_PROXY=${S_PROXY}     \
+       --build-arg GOPROXY=${GOPROXY}     \
+       .
