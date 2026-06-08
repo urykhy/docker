720c361e842e2be5775f3bc66edf97d540522d3b
Yury Stankevich <urykhy@gmail.com>
2026-06-08 21:08:34 +0300

    temporal: patched

--- temporal/example/workflows.py
+++ temporal/example/workflows.py
@@ -11,23 +11,24 @@ class MyFlow:
     @workflow.run
     async def run(self, params) -> str:
         x = []
-        x.append(await workflow.execute_activity(step_hello, params, start_to_close_timeout=timedelta(seconds=5)))
+        if workflow.patched('v1'):
+            x.append(await workflow.execute_activity(step_hello, params, start_to_close_timeout=timedelta(seconds=5)))
 
-        w = []
-        for t in params["tasks"]:
-            w.append(
-                workflow.start_activity(
-                    step_diff, t, start_to_close_timeout=timedelta(seconds=5), heartbeat_timeout=timedelta(seconds=1)
+            w = []
+            for t in params["tasks"]:
+                w.append(
+                    workflow.start_activity(
+                        step_diff, t, start_to_close_timeout=timedelta(seconds=5), heartbeat_timeout=timedelta(seconds=1)
+                    )
                 )
-            )
-        for t in w:
-            x.append(await t)
+            for t in w:
+                x.append(await t)
 
-        w = []
-        for t in params["tasks"]:
-            w.append(workflow.start_activity(step_process, t, start_to_close_timeout=timedelta(seconds=5)))
-        for t in w:
-            x.append(await t)
+            w = []
+            for t in params["tasks"]:
+                w.append(workflow.start_activity(step_process, t, start_to_close_timeout=timedelta(seconds=5)))
+            for t in w:
+                x.append(await t)
 
-        x.append(await workflow.execute_activity(step_notify, params, start_to_close_timeout=timedelta(seconds=5)))
+            x.append(await workflow.execute_activity(step_notify, params, start_to_close_timeout=timedelta(seconds=5)))
         return ",".join(x)
